87c83d5760518990055e77daf645c21384bab754
Dave <davidotoo09@gmail.com>
2025-07-05 00:08:45 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+//Number Guesser code down here:
+
+const generateTarget = () =>{
+   return Math.floor(Math.random() * 10);
+}
+
+
+
