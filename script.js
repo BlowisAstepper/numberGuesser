af57203c8511aa1b9aa718802692fef74cc4a0e0
Dave <davidotoo09@gmail.com>
2025-07-05 01:13:30 +0000

    Your changes

--- script.js
+++ script.js
@@ -8,5 +8,29 @@ const generateTarget = () =>{
    return Math.floor(Math.random() * 10);
 }
 
+function compareGuesses (humanGuess, computerGuess, secretNum){
+    // Calculate how far each guess is from the target
+    const humanDistance = Math.abs(secretNum - humanGuess);
+    const computerDistance = Math.abs(secretNum - computerGuess);
 
+    // Compare distances - human wins if tied or closer
+    if (humanDistance <= computerDistance) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
+const updateScore = (winner) =>{
+    const humnaScore = '';
+    const compterScore = '';
+      if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () =>{
+    currentRoundNumber = currentRoundNumber + 1;
+}
